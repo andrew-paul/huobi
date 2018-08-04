ee546e47b49338a0967425b7a64629857151236b
andrew-paul <42085974+andrew-paul@users.noreply.github.com>
2018-08-04 16:23:05 +0800

    Delete testhuo.py

--- testhuo.py
+++
@@ -1,9 +0,0 @@
-dict={'banners': [{'bannerId': '297e4f795ab793c8015ab79f23020000 ', 'image': '/images/ads/1489139343226.jpg'}], 'msg': '成功', 'status': 0}
-
-dict1=dict['banners']
-
-list=dict1
-
-dict2=list[0]
-
-print (dict2.values())
